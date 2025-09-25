273c47625268b9c1f1e77901b5b90130ec061504
Aditya Patidar <adityapatidar25102002@gmail.com>
2025-09-25 15:43:51 +0530

    day8

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup, login ,logOut} from "../controllers/authController.js";
+import { signup, login ,logOut,ResetPassword,SendOTP} from "../controllers/authController.js";
+import { Protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.get("/logOut", logOut);
+
+router.patch("/resetpassword", Protect, ResetPassword);
+
+router.post("/sendOtp",SendOTP);
 
 export default router;
